be2c95c11c6335c58ebe44d0a5e3a2d355b63d37
ndk <navnathkadam284@gmail.com>
2025-03-09 00:57:33 +0530

    Adding js refresh fn

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -40,3 +40,31 @@ async function get_dashboard() {
     const data = await response.json();
     alert(`${data.message} and token is ${data.token}`);
 }
+
+// refresh the token
+async function refresh_token() {
+    const refresh_token = localStorage.getItem("refresh_token");
+
+    // if the refresh token is not present then it will alert the user to login again
+    if (!refresh_token) {
+        alert("Login required");
+        return;
+    }
+
+    // if the refresh token is present then it will refresh the token
+    const response = await fetch("http://127.0.0.1:5000/refresh", {
+        method: "POST",
+        headers: { Authorization: `Bearer ${refresh_token}` },
+    });
+
+    const data = await response.json();
+
+    // if the token is refreshed successfully then it will store the new access token in the local storage
+    if (response.ok && data.access_token) {
+        localStorage.setItem("access_token", data.access_token);
+    } 
+    // if the token is not refreshed successfully then it will alert the user
+    else {
+        alert("Token refresh failed");
+    }
+}
